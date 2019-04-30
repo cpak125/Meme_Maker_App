e9e6433d3bb39e4f6b2b1ab21d18cbd1ca364e34
Chris Pak <chris.pak125@gmail.com>
2019-04-30 16:35:45 -0400

    Added styling and created photos array

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,5 +1,30 @@
 import React, { Component } from 'react'
 
+const photos = [
+    {src: '/images/boy.png'},
+    {src: '/images/crying.png'},
+    {src: '/images/dank.png'},
+    {src: '/images/devilgirl.jpg'},
+    {src: '/images/dog.png'},
+    {src: '/images/doubt.png'},
+    {src: '/images/frust.png'},
+    {src: '/images/fry.jpg'},
+    {src: '/images/bird.png'},
+    {src: '/images/jobs.jpg'},
+    {src: '/images/ned.jpeg'},
+    {src: '/images/oldie.png'},
+    {src: '/images/one-does-not.jpg'},
+    {src: '/images/penguin.png'},
+    {src: '/images/phone.jpg'},
+    {src: '/images/sad.png'},
+    {src: '/images/sponge.png'},
+    {src: '/images/trump.jpg'},
+    {src: '/images/vict-baby.png'},
+    {src: '/images/seal.png'},
+    {src: '/images/wolf.png'},
+
+]
+
 export default class MainPage extends Component {
   render() {
     return (
