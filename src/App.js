d27a437e1ca07d7f304bffc1c0ce4d5bde56dfdc
Chris Pak <chris.pak125@gmail.com>
2019-05-02 14:13:42 -0400

    Able to move text around, styled image in modal width, able to download meme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import MainPage from './Components/MainPage'
 
-function App() {
-  return (
-    <div className="App">
-      <MainPage />
-    </div>
-  )
+
+
+export default class App extends Component {
+  render() {
+    return (
+     <MainPage />
+    )
+  }
 }
 
-export default App
--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -22,7 +22,7 @@ const photos = [
     { src: '/images/trump.jpg' },
     { src: '/images/vict-baby.png' },
     { src: '/images/seal.png' },
-    { src: '/images/wolf.png' },
+    { src: '/images/wolf.png' }
 ]
 
 const initialState = {
@@ -47,37 +47,125 @@ export default class MainPage extends Component {
         ...initialState
     }
 
-    getBase64Image = (img) => {
-        // This function converts the image to data URI
-        const canvas = document.createElement("canvas")
-        canvas.width = img.width
-        canvas.height = img.height
-        const context = canvas.getContext("2d")
-        context.drawImage(img, 0, 0)
-        const dataURL = canvas.toDataURL("image/png")
-        return dataURL
-    }
-
     openImage = (index) => {
         const image = photos[index]
         const baseImage = new Image()
         baseImage.src = image.src
-        const currentImageBase64 = this.getBase64Image(baseImage)
+        const base64 = this.getBase64Image(baseImage)
         // Setting the currently selected image on the state
         this.setState(prevState => ({
             currentImage: index,
             modalIsOpen: !prevState.modalIsOpen,
-            currentImageBase64,
+            currentImageBase64: base64,
             ...initialState
         }))
     }
 
     toggle = () => {
         this.setState(prevState => ({
-          modalIsOpen: !prevState.modalIsOpen
-        }));
-      }
+            modalIsOpen: !prevState.modalIsOpen
+        }))
+    }
 
+    getBase64Image(img) {
+        // This function converts the image to data URI
+        var canvas = document.createElement("canvas")
+        canvas.width = img.width
+        canvas.height = img.height
+        var context = canvas.getContext("2d")
+        context.drawImage(img, 0, 0)
+        var dataURL = canvas.toDataURL("image/png")
+        return dataURL
+    }
+
+    // capture the user’s meme caption here, and mutate the state.
+    changeText = (event) => {
+        this.setState({
+            [event.currentTarget.name]: event.currentTarget.value
+        })
+    }
+
+    getStateObj = (e, type) => {
+        // This getBoundingClientRect() returns height, width and positions of the element
+        // In our case, we get the image's positions in the DOM since we position the text on the image.
+        let rect = this.imageRef.getBoundingClientRect()
+        // This calculation yields us the current x and y positions of the element/cursor.
+        const xOffset = e.clientX - rect.left
+        const yOffset = e.clientY - rect.top
+        // This is common function for top and bottom captions.
+        let stateObj = {}
+        if (type === "bottom") {
+            stateObj = {
+                isBottomDragging: true,
+                isTopDragging: false,
+                bottomX: `${xOffset}px`,
+                bottomY: `${yOffset}px`
+            }
+        } else if (type === "top") {
+            stateObj = {
+                isTopDragging: true,
+                isBottomDragging: false,
+                topX: `${xOffset}px`,
+                topY: `${yOffset}px`
+            }
+        }
+        return stateObj
+    }
+
+    handleMouseDown = (e, type) => {
+        // Finding current coordinates of the dragged <text />
+        const stateObj = this.getStateObj(e, type)
+        // Start tracking the mouse movement.
+        document.addEventListener('mousemove', (event) => this.handleMouseMove(event, type))
+        this.setState({
+            ...stateObj
+        })
+    }
+
+    handleMouseMove = (e, type) => {
+        // Only if dragging is active in the state, track mouse movements.
+        if (this.state.isTopDragging || this.state.isBottomDragging) {
+            let stateObj = {}
+            if (type === "bottom" && this.state.isBottomDragging) {
+                stateObj = this.getStateObj(e, type) // Getting the coordinates for bottom caption
+            } else if (type === "top" && this.state.isTopDragging) {
+                stateObj = this.getStateObj(e, type) // Getting the coordinates for top caption
+            }
+            this.setState({
+                ...stateObj
+            })
+        }
+    }
+
+    handleMouseUp = (event) => {
+        // If mouse is released, remove the event listener and terminate drag actions.
+        document.removeEventListener('mousemove', this.handleMouseMove)
+        this.setState({
+            isTopDragging: false,
+            isBottomDragging: false
+        })
+    }
+
+    convertSvgToImage = () => {
+        const svg = this.svgRef;
+        let svgData = new XMLSerializer().serializeToString(svg);
+        const canvas = document.createElement("canvas");
+        canvas.setAttribute("id", "canvas");
+        const svgSize = svg.getBoundingClientRect();
+        canvas.width = svgSize.width;
+        canvas.height = svgSize.height;
+        const img = document.createElement("img");
+        img.setAttribute("src", "data:image/svg+xml;base64," + btoa(unescape(encodeURIComponent(svgData))));
+        img.onload = function() {
+          canvas.getContext("2d").drawImage(img, 0, 0);
+          const canvasdata = canvas.toDataURL("image/png");
+          const a = document.createElement("a");
+          a.download = "meme.png";
+          a.href = canvasdata;
+          document.body.appendChild(a);
+          a.click();
+        };
+      }
 
     render() {
         const image = photos[this.state.currentImage]
@@ -85,9 +173,9 @@ export default class MainPage extends Component {
         baseImage.src = image.src
         // To avoid stretching and compressing each image, fix the aspect ratio.
         // Fix the width to 600 and calculate height based on width-height ratio
-        const wrh = baseImage.width / baseImage.height
-        const newWidth = 600
-        const newHeight = newWidth / wrh
+        var wrh = baseImage.width / baseImage.height
+        var newWidth = 600
+        var newHeight = newWidth / wrh
         const textStyle = {
             fontFamily: "Impact",
             fontSize: "50px",
@@ -99,29 +187,41 @@ export default class MainPage extends Component {
 
 
         return (
-            <div className="content">
-                {/* Create the image gallery here! */}
-                {photos.map((image, index) => (
-                    <div className="image-holder" key={image.src}>
-                        <span className="meme-top-caption">Top text</span>
-                        <img
-                            style={{
-                                width: "100%",
-                                cursor: "pointer",
-                                height: "100%"
-                            }}
-                            alt={index}
-                            src={image.src}
-                            // The onClick here determines current image
-                            onClick={() => this.openImage(index)}
-                            role="presentation"
-                        />
-                        <span className="meme-bottom-caption">Bottom text</span>
+            <div>
+                <div className="main-content">
+                    <div className="sidebar">
+                        <NavbarBrand href="/">Make-a-Meme</NavbarBrand>
+                        <p>
+                            This is a fun 5 hour project inspired by imgur. Built with React.
+                        </p>
+                        <p>
+                            You can add top and bottom text to a meme-template, move the text around and can save the image by downloading it.
+                        </p>
                     </div>
-                ))}
-
+                    <div className="content">
+                        {/* Create the image gallery here! */}
+                        {photos.map((image, index) => (
+                            <div className="image-holder" key={image.src}>
+                                <span className="meme-top-caption">Top text</span>
+                                <img
+                                    style={{
+                                        width: "100%",
+                                        cursor: "pointer",
+                                        height: "100%"
+                                    }}
+                                    alt={index}
+                                    src={image.src}
+                                    // The onClick here determines current image
+                                    onClick={() => this.openImage(index)}
+                                    role="presentation"
+                                />
+                                <span className="meme-bottom-caption">Bottom text</span>
+                            </div>
+                        ))}
+                    </div>
+                </div>
                 {/*  The workstation/modal for meme creation */}
-                <Modal className="meme-gen-modal" isOpen={this.state.modalIsOpen}>
+                <Modal centered={true} size="lg" isOpen={this.state.modalIsOpen}>
                     <ModalHeader toggle={this.toggle}>Make-a-Meme</ModalHeader>
                     <ModalBody>
                         <svg
@@ -163,11 +263,11 @@ export default class MainPage extends Component {
                         <div className="meme-form">
                             <FormGroup>
                                 <Label for="toptext">Top Text</Label>
-                                <input className="form-control" type="text" name="toptext" id="toptext" placeholder="Add text to the top" onChange={this.changeText} />
+                                <input className="form-control" type="text" name="toptext" id="toptext" placeholder="Add top text" onChange={this.changeText} />
                             </FormGroup>
                             <FormGroup>
                                 <Label for="bottomtext">Bottom Text</Label>
-                                <input className="form-control" type="text" name="bottomtext" id="bottomtext" placeholder="Add text to the bottom" onChange={this.changeText} />
+                                <input className="form-control" type="text" name="bottomtext" id="bottomtext" placeholder="Add bottom text" onChange={this.changeText} />
                             </FormGroup>
                             <button onClick={() => this.convertSvgToImage()} className="btn btn-primary">Download Meme!</button>
                         </div>
